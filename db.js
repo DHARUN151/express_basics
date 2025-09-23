c8131a793a7e70a41a63b2023bd8fa2b9e43db13
DHARUN KUMAR <83487143+DHARUN151@users.noreply.github.com>
2025-09-23 22:23:49 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,10 +2,11 @@
 const mongoose = require('mongoose');
 
 async function connection() {
-    const connection = await mongoose.connect('mongodb+srv://dharuns2006:OaMYql94va91H8nB@cluster0.jdjg6qw.mongodb.net/');
+    const connection = await mongoose.connect('');
     console.log("connected to db");
 
     
 }
+
 
 module.exports = connection;
